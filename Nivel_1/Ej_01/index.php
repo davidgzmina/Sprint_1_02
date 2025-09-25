0b7fd298e26112ff21d35e1282923ec1786e77fc
davidgzmina <davidmobile28@gmail.com>
2025-09-25 19:39:38 +0200

    exercise 1 of level 1 done

--- Nivel_1/Ej_01/index.php
+++ Nivel_1/Ej_01/index.php
@@ -4,11 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #0e2424ff;
+            margin: 20px;
+        }
+        h1, h2, h3 {
+            color: #ffffffff;
+        }
+    </style>
 </head>
 <body>
+
+
+
     <?php
-    
-    
+        const NOMBRE="DAVID";
+        $apellido = "Gonzalez";
+        $edad = 30;
+        $mayorEdad = true;
+        $altura = 1.72;
+
+         echo "<h1>Hola mi nombre es: " . NOMBRE . " ." ."</h1>";
+
+         echo "<h2> Mi apellido es $apellido , tengo $edad años y mido $altura . </h2>";
+
+        if($mayorEdad>=18){
+            echo "<h3> Y soy mayor de edad. </h3>";
+        } else {
+            echo "<h3> Y no soy mayor de edad. </h3>";
+        }
     
     
     
