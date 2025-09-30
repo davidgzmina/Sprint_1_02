8d989cd53b161a8e7b26e248fb8e3da889b9a025
davidgzmina <davidmobile28@gmail.com>
2025-09-30 13:40:00 +0200

    nuevo README.md con explicaciones sobre la estructura del proyecto

---
+++ Nivel_3/Ej_01/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sprint_01_tema_02_Nv_3_ej_01</title>
+
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: cadetblue;
+            font-size: 30px;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    /*
+    El cribado de Eratóstenes (o criba de Eratóstenes) es un algoritmo diseñado para encontrar números primos dentro de un intervalo dado.
+    Basándote en la información del enlace adjunto, implementa el cribado de Eratóstenes dentro de una función,
+    de forma que podamos invocar la función con un número concreto.
+    */
+
+
+
+
+
+
+
+
+
+
+    ?>
+</body>
+</html>
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Sprint_1_02
 Repositorio del sprint 1 tema 2 "PHP BASICS" del bootcamp full stack php
+
+#Todos los ejercicios estan en subcarpetas dentro de carpetas que los separan por niveles.
+
+#Ejemplo de estructura Sprint_1_02 --> Nivel_1 --> Ej_01
+
+#Tecnologia usada html, csss, y php.
