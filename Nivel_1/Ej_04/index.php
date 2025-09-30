fbfe6767f3a4175322732cd17982c48e82339bd2
davidgzmina <davidmobile28@gmail.com>
2025-09-30 13:34:47 +0200

    Ej_04 nivel 1 fixed: logica metida dentro de la funcion

--- Nivel_1/Ej_04/index.php
+++ Nivel_1/Ej_04/index.php
@@ -23,25 +23,30 @@
     La cuenta debe mostrarse en pantalla paso a paso.
     */
         
-
+        $a = 0;
         $limite = 10;
         $numCuenta = 2;
-        $numVeces = $limite / $numCuenta;
 
-       
-        $resultado = "Has alcanzado tu limite. Haciendo un total de $numVeces cuentas.";
+        echo "<h1>Vamos a contar de $numCuenta en $numCuenta hasta que llegues a $limite que es tu limite:<br></h1>";
 
-        echo "<h1>Vamos a contar de $numCuenta en $numCuenta hasta que llegues a $limite que es tu limite.<br></h1>";
+        function contador ($limite, $numCuenta){
 
-            for($i = 0; $i <= $limite; $i += $numCuenta){
+            $numVeces = $limite / $numCuenta;
 
-                echo "En esta cuenta llevas = $i.<br>";
-
+            for($i = 0; $i <= $limite; $i += $numCuenta)
+            {
+               echo "En esta cuenta llevas = $i.<br>";
             }
-             
+
+            return  "<br>Has alcanzado tu limite. Haciendo un total de $numVeces cuentas.";
+        }
 
 
-        echo "<h2>$resultado</h2>";
+        $resultado = contador($limite,$numCuenta);
+
+        echo $resultado;
+
+
 
 
     ?>
