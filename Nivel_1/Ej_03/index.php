2af524b717d6ea3ea32ae520195655e6be07a879
davidgzmina <davidmobile28@gmail.com>
2025-09-30 12:29:11 +0200

    Ej_03 nivel 1 apartado b fixed logica de la programacion metida en la funcion tal como pide el enunciado

--- Nivel_1/Ej_03/index.php
+++ Nivel_1/Ej_03/index.php
@@ -19,8 +19,6 @@
 </head>
 <body>
 
-
-
     <?php
         $X = 1;
         $Y = 2;
@@ -35,7 +33,7 @@
         $resultado;
        
         // Parte A
-        echo  " El valor de la variable X es = $X y el valor de la variable Y es = $Y. <br>";
+        echo "El valor de la variable X es = $X y el valor de la variable Y es = $Y. <br>";
         echo "La SUMA de las variables X e Y es de = " . $X + $Y . "<br>";
         echo "La RESTA de las variables X e Y es de = " . $X - $Y . "<br>";
         echo "El PRODUCTO de las variables X e Y es de = " . $X * $Y . "<br>";
@@ -52,35 +50,23 @@
         echo "La SUMA de todas variables es de = " . $N + $M + $X + $Y . "<br>";
         echo "El PRODUCTO de todas variables es de = " . $N * $M * $X * $Y . "<br>";
 
-        // Parte b
-        $resultado = match($operacion){
-            "suma"=> "El resultado de la suma es = " . $a + $b,
-            "resta"=> "El resultado de la resta es = " . $a - $b,
-            "multiplicacion"=>"El resultado de la multiplicacion es = " .  $a * $b,
-            "division"=> "El resultado de la division es = " . $a / $b,
-            default=> "Esa operacion no es posible",
-        };
-
+        // Parte B
+        function calculadora($a, $b, $operacion){
+            $resultado = match($operacion){
+                "suma"=> "El resultado de la suma es = " . $a + $b,
+                "resta"=> "El resultado de la resta es = " . $a - $b,
+                "multiplicacion"=>"El resultado de la multiplicacion es = " .  $a * $b,
+                "division"=> "El resultado de la division es = " . $a / $b,
+                default=> "Esa operacion no es posible",
+            };
+            return $resultado;
+        }
+        $resultado = calculadora($a, $b, $operacion);
 
         echo "<h2>$resultado</h2>";
 
 
-
-
-
-
-
-
-
         ?>
-
-
-
-
-
-       
-      
-
 
 
 </body>
