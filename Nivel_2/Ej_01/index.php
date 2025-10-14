ce9db712118e7ebfaab3d7304b3b7ae68baab2ca
davidgzmina <davidmobile28@gmail.com>
2025-10-14 13:17:42 +0200

    Fixed default parameter

--- Nivel_2/Ej_01/index.php
+++ Nivel_2/Ej_01/index.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sprint_01_tema_02_Nv_2_ej_01</title>
 
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: cadetblue;
-            font-size: 30px;
-            color: white;
-        }
-    </style>
-</head>
-<body>
 <?php
 
 /*
@@ -23,20 +7,19 @@ Toda llamada que dure menos de 3 minutos tiene un coste de 10 céntimos.
 Cada minuto adicional a partir de los 3 primeros es un paso de contador y cuesta 5 céntimos.
 */
 
-$minuto = 1;
-$tarifa = 5;
+$minuto = 8;
 
 
-
-
-function moneyCall ($minuto,$tarifa){
+function moneyCall ($minuto,$tarifa = 5){
     $tarifaBase = 10;
 
-    if($minuto<=3){
+    if($minuto<=3)
+    {
         return $tarifaBase;
     }
 
-    else {
+    else 
+    {
 
         $minutosAdicionales = $minuto - 3;
         $dinero = ($minutosAdicionales * $tarifa) + $tarifaBase ;
@@ -46,11 +29,9 @@ function moneyCall ($minuto,$tarifa){
 
 }
 
-//Prueba => $resultado = moneyCall(8,5);
-$resultado = moneyCall($minuto,$tarifa);
+
+$resultado = moneyCall($minuto);
 
 echo "El coste de tu llamada es de $resultado centimos.";
 
 ?>
-</body>
-</html>
