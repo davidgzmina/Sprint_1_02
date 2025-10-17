d0ea1c5687feb4b0dc031a4a61012731ed05d9e1
davidgzmina <davidmobile28@gmail.com>
2025-10-17 11:34:21 +0200

    Arreglo del return en la variable rango +  creacion de distintas funciones para dejarlo mejor + guardar las puntuaciones de las partidas en un array

--- Nivel_2/Ej_02/index.php
+++ Nivel_2/Ej_02/index.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sprint_01_tema_02_Nv_2_ej_02</title>
 
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: cadetblue;
-            font-size: 30px;
-            color: white;
-        }
-    </style>
-</head>
-<body>
 <?php    
 /*
 Imagina que estás programando parte de la lógica de la clasificación de puntos de un juego.
@@ -26,61 +10,64 @@ De estas puntuaciones necesitaremos:
 Piensa en hacer más de una función para resolver este problema.
 
 */
+/*Sobre el ejercicio 2, has pensado en usar algún array para mejorarlo?  La variable "$partidas" guarda partidas o el número de partidas? 
+Por otra parte, hace falta que hagas 3 veces return de la variable $rango?*/
 
-$partidas = 3;
-$partida1 = 950;
-$partida2 = 2500;
-$partida3 = 4500;
 
-function calcularPuntuacion($partida1,$partida2,$partida3){
 
-    $puntuacionPartidas = $partida1 + $partida2 + $partida3;
+$partidas =  [950,2500,4500];
+
+function calcularPuntuacion($partidas)
+{
+
+    $puntuacionPartidas = array_sum($partidas);
 
     return $puntuacionPartidas;
 
 }
 
-function calcularMedia($puntuacionPartidas,$partidas){
+function calcularMedia($puntuacionPartidas)
+{
+    $numPartidas = 3;
+    $mediaPartidas = $puntuacionPartidas /$numPartidas;
 
-     $mediaPartidas = $puntuacionPartidas /$partidas;
-
-     return $mediaPartidas;
+    return $mediaPartidas;
 }
 
-function clasificarJugador($puntuacionPartidas){
+function clasificarJugador($puntuacionPartidas)
+{
 
     $rango = "";
 
-    if($puntuacionPartidas>8000){
-       $rango = "Profesional";
-       return $rango; 
+    if($puntuacionPartidas>8000)
+    {
+       $rango = "Profesional"; 
     }
-    else if ($puntuacionPartidas<=8000 && $puntuacionPartidas>=4000){
+    else if ($puntuacionPartidas<=8000 && $puntuacionPartidas>=4000)
+    {
         $rango = "Intermedio";
-        return $rango; 
     }
-    else{
+    else
+    {
         $rango = "Principiante";
-        return $rango; 
     }
 
+    return $rango; 
 
 }
 
-$puntuacionPartidas = calcularPuntuacion($partida1,$partida2,$partida3);
-$mediaPartidas = calcularMedia($puntuacionPartidas,$partidas);
+$puntuacionPartidas = calcularPuntuacion($partidas);
+$mediaPartidas = calcularMedia($puntuacionPartidas);
 $rango = clasificarJugador($puntuacionPartidas);
 
 
 
-$resultado = "Rango: $rango. <br> Puntos totales: $puntuacionPartidas. <br>  Media por partida: $mediaPartidas.<br>
-Resultado partida 1: $partida1.<br>
-Resultado partida 2: $partida2.<br>
-Resultado partida 3: $partida3.<br>";
+$resultado = "Rango: $rango" . PHP_EOL . "Puntos totales: $puntuacionPartidas.". PHP_EOL . "Media por partida: $mediaPartidas." . PHP_EOL . 
+"Resultado partida 1: $partidas[0]." . PHP_EOL .
+"Resultado partida 2: $partidas[1]." . PHP_EOL .
+"Resultado partida 3: $partidas[2].";
 
 
 echo $resultado;
 
 ?>
-</body>
-</html>
